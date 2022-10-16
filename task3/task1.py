15744b7980ca35953941c04e3a3204e934107e48
NIKITA-MAC <nekitos99956@yandex.ru>
2022-10-16 20:29:31 +0400

    v1

--- task3/task1.py
+++ task3/task1.py
@@ -4,9 +4,9 @@ import random
 list1 = []
 sum = 0
 print('На нечётных позициях элементы: ',  end = '')
-for i in range(random.randrange(10,20)):
+for i in range(5):
     list1.append(random.randrange(20,100))
-    if (list1[i] % 2 != 0):
+    if (i % 2 != 0):
         print(list1[i], end = ' ')
         sum += list1[i]
 print('\nsum = ', sum)
